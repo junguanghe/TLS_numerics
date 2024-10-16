69de751164b6257ff1c054e7a8b884d40ff39f3d
Wei-Ting Lin <linwaytin@gmail.com>
2024-10-16 15:27:35 -0500

    Add SelfEnergy class for iteration.

--- src/calc_2nd_self_energy.py
+++ src/calc_2nd_self_energy.py
@@ -5,15 +5,18 @@ import numpy as np
 multp = 30
 n_min = 100
 
-def Sigma_eps(n, T, E, Delta, Gamma, Nge=0.5):
+def SE0(self, n, T, E, Delta, Gamma, Nge=0.5):
     n_limit = np.abs(n)*30 + n_min
     l = np.arange(-n_limit, n_limit)
     eps_l = 2*np.pi*T*(l+1/2)
 
     kernel = E/(E**2 + (2*np.pi*T)**2*(n-l)**2)
-    denom = np.sqrt(Delta**2 + eps_l**2)
-    prop_eps = -1j*eps_l/denom
-    prop_delta = Delta/denom
+    SE_eps_l = self.get_SE_eps(l) if self!=None else 0
+    SE_delta_l = self.get_SE_delta(l) if self!=None else 0
+
+    denom = np.sqrt((Delta + SE_delta_l)**2 + (eps_l + 1j*SE_eps_l)**2)
+    prop_eps = (-1j*eps_l + SE_eps_l)/denom
+    prop_delta = (Delta + SE_delta_l)/denom
 
     se_eps = np.sum(kernel*prop_eps)
     se_delta = np.sum(kernel*prop_delta)
@@ -27,29 +30,22 @@ def Sigma_eps(n, T, E, Delta, Gamma, Nge=0.5):
         eps_l_add = 2*np.pi*T*(l_add+1/2)
 
         kernel_add = E/(E**2 + (2*np.pi*T)**2*(n-l_add)**2)
-        denom_add = np.sqrt(Delta**2 + eps_l_add**2)
-        prop_eps_add = -1j*eps_l_add/denom_add
-        prop_delta_add = Delta/denom_add
+        SE_eps_l_add = self.get_SE_eps(l_add) if self!=None else 0
+        SE_delta_l_add = self.get_SE_delta(l_add) if self!=None else 0
+
+        denom_add = np.sqrt((Delta+SE_delta_l_add)**2 + (eps_l_add+1j*SE_eps_l_add)**2)
+        prop_eps_add = (-1j*eps_l_add+SE_eps_l_add)/denom_add
+        prop_delta_add = (Delta+SE_delta_l_add)/denom_add
 
         se_eps_add = np.sum(kernel_add*prop_eps_add)
         se_delta_add = np.sum(kernel_add*prop_delta_add)
 
 
-        # eps_l_p = 2*np.pi*T*(l_p+1/2)
-        # eps_l_m = 2*np.pi*T*(l_m+1/2)
-
-        # kernel_p = E/(E**2 + (2*np.pi*T)**2*(n-l_p)**2)
-        # kernel_m = E/(E**2 + (2*np.pi*T)**2*(n-l_m)**2)
-        # prop_p = -1j*eps_l_p/np.sqrt(Delta**2 + eps_l_p**2)
-        # prop_m = -1j*eps_l_m/np.sqrt(Delta**2 + eps_l_m**2)
-
-        # s_next = np.sum(kernel_p*prop_p) + np.sum(kernel_m*prop_m)
-
         se_eps += se_eps_add
         se_delta += se_delta_add
 
         if np.abs((se_eps_add+se_delta_add)/(se_eps+se_delta)) < 1e-4:
-            print(f"Current iter: {i}")
+            # print(f"Current iter: {i}")
             break
 
     return Gamma * Nge * T * se_eps, Gamma * Nge * T * se_delta
@@ -62,3 +58,46 @@ def Sigma_eps(n, T, E, Delta, Gamma, Nge=0.5):
 def SE_asymptotic(T, E, Delta, Gamma, Nge=0.5):
     a = E/(2*np.pi*T)
     return Gamma*Nge*(T/E)*a**2*(np.pi/(a*np.tanh(a*np.pi)))
+
+# Create a class to save the function Σ(n)
+# so that it can be evaluated for the RHS of the fixed point problem.
+class SelfEnergy:
+    def __init__(self, T, E, Delta, Gamma, Nge, n_grid, SE_eps, SE_delta):
+        self.T = T
+        self.E = E
+        self.Delta = Delta
+        self.Gamma = Gamma
+        self.Nge = Nge
+
+        self.n_grid = n_grid
+        self.SE_eps = SE_eps
+        self.SE_delta = SE_delta
+        self.n_min = n_grid[0]
+        self.n_max = n_grid[-1]
+
+
+    def __call__(n):
+        pass
+
+    def get_SE_eps(self, ls):
+        """
+        ls is the array of Matsubara index. It must be sorted.  
+        """
+        res = np.fromiter((self.SE_eps[l-self.n_min] if self.n_min <= l <= self.n_max else
+                           1j*SE_asymptotic(self.T, self.E, self.Delta, self.Gamma, self.Nge) if l < self.n_min else
+                           -1j*SE_asymptotic(self.T, self.E, self.Delta, self.Gamma, self.Nge) for l in ls),
+                          dtype=complex)
+        return res
+
+    def get_SE_delta(self, ls):
+        """
+        ls is the array of Matsubara index. It must be sorted.  
+        """
+        res = np.fromiter((self.SE_delta[l-self.n_min] if self.n_min <= l <= self.n_max else
+                           0 if l < self.n_min else
+                           0 for l in ls),
+                          dtype=complex)
+        return res
+
+    # Bind the method
+    eval_RHS = SE0
--- src/plot_2nd_SE.py
+++ src/plot_2nd_SE.py
@@ -2,24 +2,40 @@
 from calc_2nd_self_energy import *
 import matplotlib.pyplot as plt
 
-ns = [*range(-500, 500)]
+ns = [*range(-800, 800)]
 
 T = 0.3
 E = 0.6
 Delta = 1.764
-Gamma = 1.0
+Gamma = 100.0
 
 SE_eps = []
 SE_delta = []
 
 for n in ns:
-    se = Sigma_eps(n, T, E, Delta, Gamma)
+    se = SE0(None, n, T, E, Delta, Gamma)
     SE_eps.append(se[0])
     SE_delta.append(se[1])
     
+SE1 = SelfEnergy(T, E, Delta, Gamma, 0.5, np.array(ns), SE_eps, SE_delta)
 
-plt.plot(ns, np.imag(SE_eps), label="epsilon")
-plt.plot(ns, np.real(SE_delta), label="Delta")
+plt.plot(ns, np.imag(SE_eps), label=r"$\Sigma_\epsilon$ (imag)")
+plt.plot(ns, np.real(SE_delta), label=r"$\Sigma_\Delta$ (real)")
+
+n_larger = np.arange(-1000, 1000)
+plt.plot(n_larger, np.imag(SE1.get_SE_eps(n_larger)), lw=0.8)
+plt.plot(n_larger, np.real(SE1.get_SE_delta(n_larger)), lw=0.8)
+
+SE1_eps = []
+SE1_delta = []
+for n in ns:
+    se = SE1.eval_RHS(n, T, E, Delta, Gamma)
+    SE1_eps.append(se[0])
+    SE1_delta.append(se[1])
+
+plt.plot(ns, np.imag(SE1_eps), label=r"$\Im\Sigma_\epsilon$, iter1")
+plt.plot(ns, np.real(SE1_delta), label=r"$\Re\Sigma_\Delta$ (real), iter1")
+
 plt.legend()
 plt.grid()
 plt.show()
